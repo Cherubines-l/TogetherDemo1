5e8dce7407cde72d62559e1df7a72a9ca3c2f57d
Canwuheng <1296959377@qq.com>
2020-03-18 19:28:38 +0800

    创建提交2

---
+++ app/src/main/java/com/example/together_demo1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.together_demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
