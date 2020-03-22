416ed52780b9678d42999683881206c9339f8375
Canwuheng <1296959377@qq.com>
2020-03-22 23:37:05 +0800

    创建首页界面和菜单Fragment

---
+++ app/src/main/java/com/example/together_demo1/Fragment/MenuFragment.java
@@ -0,0 +1,23 @@
+package com.example.together_demo1.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class MenuFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
