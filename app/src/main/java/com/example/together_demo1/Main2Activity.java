63c15c7cb7f77bcdaa6122930a08805fb5c89527
Canwuheng <1296959377@qq.com>
2020-03-24 23:11:44 +0800

    框架前端1

---
+++ app/src/main/java/com/example/together_demo1/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.together_demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
